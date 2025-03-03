8ce97ccf6986133906c88364d809f53ec84e6eda
DaniilT048 <tis.daniil98@gmail.com>
2025-03-03 08:34:11 +0200

    truth or falsy testing

--- main.spec.js
+++ main.spec.js
@@ -20,8 +20,22 @@ it('not day of week', () => {
 
 it('valid age', () => {
     expect(ageClassification(1)).toBe('Дитинство');
+    expect(ageClassification(25)).toBe('Молодість');
+    expect(ageClassification(45)).toBe('Зрілість');
 })
 
 it('invalid age', () => {
     expect(ageClassification(-1)).toBeNull();
+    expect(ageClassification(0)).toBeNull();
+    expect(ageClassification(125)).toBeNull();
+})
+
+it('truth valid', () => {
+    expect(ageClassification(-1) === null).toBeTruthy();
+    expect(ageClassification(20) === 'Дитинство').toBeTruthy();
+})
+
+it('falsy valid', () => {
+    expect(ageClassification(-1) === 'Дитинство').toBeFalsy();
+    expect(ageClassification(20) === null).toBeFalsy();
 })
